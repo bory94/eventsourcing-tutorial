7cb6a19049d365d21fa4de5058a891b975f98505
Hyungsuk Kim <hskim@drmamma.co.kr>
2023-01-09 20:21:36 +0900

    Fix invalid annotation

--- src/main/kotlin/com/bory/eventsourcingtutorial/core/infrastructure/config/web/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/bory/eventsourcingtutorial/core/infrastructure/config/web/GlobalExceptionHandler.kt
@@ -1,12 +1,12 @@
 package com.bory.eventsourcingtutorial.core.infrastructure.config.web
 
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.context.request.ServletWebRequest
 import javax.validation.ConstraintViolationException
 
-@ControllerAdvice
+@RestControllerAdvice
 class GlobalExceptionHandler {
     @ExceptionHandler(value = [ConstraintViolationException::class])
     fun handleConstraintViolationException(
